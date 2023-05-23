553720aacde109c2d10ada820c264e227dd140cc
Sam <sam@Sams-MacBook-Air.local>
2023-05-23 14:50:00 -0400

    added Ai to follow character

---
+++ Assets/AiLocomotion.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AiLocomotion : MonoBehaviour
+{
+    public Transform playerTransform;
+    NavMeshAgent agent;   
+     Animator animator;
+    public float maxTime = 1.0f;
+    public float maxDistance = 1.0f;
+    float timer = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0.0f)
+        {
+            float sqrDistance = (playerTransform.position - agent.destination).sqrMagnitude;
+            if (sqrDistance > maxDistance*maxDistance)
+            {
+                agent.destination = playerTransform.position;
+            }
+            timer = maxTime;
+        }
+       
+        animator.SetFloat("speed", agent.velocity.magnitude);
+    }
+}
